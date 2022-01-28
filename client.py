7e54601263cdd1138a76b6fb3f0f6ea3b49a69e8
Toni59400 <95470541+Toni59400@users.noreply.github.com>
2022-01-28 10:47:23 +0100

    client

--- client.py
+++ client.py
@@ -38,6 +38,40 @@ class Client() :
     '''
     Mise à jour des données dans la bd
     '''
+    def maj_data(self) :
+        cursor.execute("UPDATE PROSPECT set nom = ? , prenom = ? , numSiret = ?, adressePostale = ?, codePostale = ?, ville = ? WHERE id_prospect = ?", self.__nom, self.__prenom, self.__numSiret, self.__adresse, self.__codePostal, self.__ville, self.__id)
+        connection.commit()
 
+    """
+    setteur de la class client
+    """
     def set_nom(self, new) : 
         self.__nom = new
+        self.maj_data()
+
+    def set_prenom(self, new) : 
+        self.__prenom = new 
+        self.maj_data()
+
+    def set_numSiret(self, new) : 
+        self.__numSiret = new
+        self.maj_data()
+
+    def set_adresse(self, new) : 
+        self.__adresse = new 
+        self.maj_data()
+
+    def set_codePostal(self, new) : 
+        self.__codePostal = new 
+        self.maj_data()
+
+    def set_ville(self, new) : 
+        self.__ville = new 
+        self.maj_data()
+
+    def affiche_client(self) : 
+        if self.__numSiret == "" : 
+            # client physique
+            return "Client Physique, {} {} habite au {} à {} {}".format(self.__prenom, self.__nom, self.__adresse, self.__codePostal, self.__ville)
+        else : 
+            return "Client Moral, {} recensé au {} à {} {}".format(self.__nom, self.__adresse, self.__codePostal, self.__ville)
